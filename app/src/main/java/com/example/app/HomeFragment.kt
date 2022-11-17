373227e64f6d84d3d179c0515a92cfa040f81b10
JEONG <phliopox327@naver.com>
2022-11-17 17:36:01 +0900

    home viewpager ui 조정

--- app/src/main/java/com/example/app/HomeFragment.kt
+++ app/src/main/java/com/example/app/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
 import org.json.JSONObject
 
@@ -41,6 +42,7 @@ class HomeFragment : Fragment() {
             val homeData = gson.fromJson<HomeData>(homeJsonString, HomeData::class.java)
             
             toolBarTitle.text = homeData.title.text
+
             GlideApp.with(this)
                 .load(homeData.title.iconUrl)
                 .into(toolbarIcon)
@@ -48,6 +50,16 @@ class HomeFragment : Fragment() {
             viewPager.adapter=HomeBannerAdapter().apply {
                 submitList(homeData.topBanners)
             }
+            val screenWidth = resources.displayMetrics.widthPixels
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            //페이지당 크기 312dp , 페이지당 간격 16dp -> 픽셀로 단위 변경
+            val offset = screenWidth - pageWidth - pageMargin
+            viewPager.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            viewPager.offscreenPageLimit = 3
+            TabLayoutMediator(viewPagerIndicator,viewPager) { tab, position ->  }.attach()
         }
     }
 }
