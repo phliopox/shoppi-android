c535baf5f566114272cb281ec83922f761624ad8
JEONG <phliopox327@naver.com>
2022-11-21 15:44:42 +0900

    세부 카테고리 json 작성 api 구성

--- app/src/main/java/com/example/app/model/Banner.kt
+++ app/src/main/java/com/example/app/model/Banner.kt
@@ -6,17 +6,18 @@ data class Banner(
     @SerializedName("background_image_url") val backgroundImageUrl :String,
     val badge: BannerBadge,
     val label: String,
-    @SerializedName("product_detail") val productDetail: ProductDetail
+    @SerializedName("product_detail") val productDetail: Product
 )
 data class BannerBadge(
     val label :String,
     @SerializedName("background_color") val backgroundColor : String
 )
-data class ProductDetail(
+data class Product(
     @SerializedName("brand_name") val brandName :String,
     val label : String,
     @SerializedName("discount_rate") val discountRate: Int,
     val price: Int,
-    @SerializedName("thumbnail_image") val thumbnailImageUrl:String,
+    @SerializedName("thumbnail_image") val thumbnailImageUrl:String?,
+    @SerializedName("representative_image_url") val representativeImageUrl : String?,
     @SerializedName("product_id") val productId:String
 )
