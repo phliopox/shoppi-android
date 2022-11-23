24215507ae0ca44cfd7d50124b03aa4895f637a0
JEONG <phliopox327@naver.com>
2022-11-23 17:47:27 +0900

    중첩 recyclerview layout 작성

--- app/src/main/java/com/example/app/ui/common/ImageBindingAdapters.kt
+++ app/src/main/java/com/example/app/ui/common/ImageBindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.example.app.ui.common
 
+import android.media.Image
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.example.app.GlideApp
@@ -9,6 +10,16 @@ fun loadImage(view : ImageView, imageUrl : String?){
     if(!imageUrl.isNullOrEmpty()) {
         GlideApp.with(view)
             .load(imageUrl)
+            .into(view)
+    }
+}
+
+@BindingAdapter("circleImageUrl")
+fun loadCircleImage(view : ImageView , imageUrl: String?){
+    if(!imageUrl.isNullOrEmpty()) {
+        GlideApp.with(view)
+            .load(imageUrl)
+            .circleCrop()
             .into(view)
     }
 }
